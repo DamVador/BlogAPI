8446ffd91d91f5ec68ce44b477b73e6fd304bff3
Damien <damienbanville@hotmail.fr>
2021-01-21 19:52:07 +0100

    devise admin and articles scaffold done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  devise_for :admins,
+  defaults: { format: :json },
+  path: '',
+  path_names: {
+    sign_in: 'api/login',
+    sign_out: 'api/logout',
+    registration: 'api/signup'
+  },
+  controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
 end
